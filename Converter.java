b779a966ff81a70b3679583c77b7919b82582765
joeoakes <jxo19@psu.edu>
2023-09-27 21:01:41 -0400

    Checking in

--- Converter.java
+++ Converter.java
@@ -8,4 +8,22 @@ public class Converter {
         return Integer.toBinaryString(decNum);
     }
 
+    public static double convertVolume(double volume, int sourceUnit, int targetUnit) {
+        switch (sourceUnit) {
+            case 1: // Cubic Meters
+                switch (targetUnit) {
+                    case 1: // Cubic Meters
+                        return volume;
+                    case 2: // Liters
+                        return volume * 1000;
+                    case 3: // Gallons
+                        return volume * 264.172;
+                    case 4: // Cubic Feet
+                        return volume * 35.3147;
+                }
+                break;
+        }
+        return 0.0;
+    }
+
 }
--- Main.java
+++ Main.java
@@ -2,27 +2,33 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Decimal value is " + Converter.convertHex2Decimal("ff"));
-        System.out.println("Decimal value is " + Converter.convertDecimal2Binary(42));
+        //Handle Base number systems
+        System.out.println("Enter a hexadecimal number: ");
+        String hexNum = scanner.next();
+        System.out.println("the Decimal value is " + Converter.convertHex2Decimal(hexNum));
+        System.out.println("Enter a decimal number: ");
+        int decimalNum = scanner.nextInt();
+        System.out.println("the Binary value is " + Converter.convertDecimal2Binary(decimalNum));
+
+        //Handle volume
+        System.out.println("Volume Conversion:");
+        System.out.println("1. Cubic Meters");
+        System.out.println("2. Liters");
+        System.out.println("3. Gallons");
+        System.out.println("4. Cubic Feet");
+        System.out.print("Enter the source volume unit (1/2/3/4): ");
+        int sourceUnit = scanner.nextInt();
+        System.out.print("Enter the volume value: ");
+        double volume = scanner.nextDouble();
+        System.out.print("Enter the target volume unit (1/2/3/4): ");
+        int targetUnit = scanner.nextInt();
+        System.out.println("Converted volume: " + Converter.convertVolume(volume, sourceUnit, targetUnit));
+
+        scanner.close();
 
     }
 
 
-
-        public static double convertVolume(double volume) {
-            Scanner scanner = new Scanner(System.in);
-            int targetUnit = scanner.nextInt();
-            switch (targetUnit) {
-                case 1: // Cubic Meters
-                    return volume / 264.172;
-                case 2: // Liters
-                    return volume * 3.78541;
-                case 3: // Gallons
-                    return volume;
-                case 4: // Cubic Feet
-                    return volume * 0.133681;
-            }
-            return 0.0;
-        }
 }
